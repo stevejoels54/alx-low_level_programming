216d5965bbe08c0818d3b4d8df99770bc7da2400
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-16 22:14:05 +0300

    Fixed 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
 * main - main function of program
 * Description: prints "Programming is like building a multilingual puzzle"
-* Return 0
+* Return: 0
 */
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
