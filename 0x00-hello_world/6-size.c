3faef2fd59cdd373743f359f7a249a3da3b533ed
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-16 22:57:01 +0300

    Fixed 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
 * main - main function of program
-* Description: Prints the size of various types on the computer it is compiled and run on
+* Description: "Prints the size of various types on the computer"
 * Return: 0
 */
 
