41ce5181aba74363a2cc2247cf3308a009307aef
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-21 23:07:35 +0300

    Fixed 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,6 +13,8 @@ void print_times_table(int n)
 int i, j, product;
 if (n < 0 || n > 15)
 {
+return;
+}
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -41,4 +43,4 @@ printf("%d", product);
 printf("\n");
 }
 }
-}
+
