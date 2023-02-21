0c3d8cae8f40d74aa95a49ef99e3d5a78ed95aa7
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-21 23:53:22 +0300

    Added 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 int num1 = 1, num2 = 2, nextTerm, sum = 0;
-    
 while (num2 <= 4000000)
 {
 if (num2 % 2 == 0)
@@ -22,7 +21,6 @@ nextTerm = num1 + num2;
 num1 = num2;
 num2 = nextTerm;
 }
-    
 printf("%d\n", sum);
 return (0);
 }
