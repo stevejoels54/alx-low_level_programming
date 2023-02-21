6f71851166f17c1b50b54fe3b9de88e206325135
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-21 16:40:36 +0300

    Fixed 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* _isalph - Entry point
+* _isalpha - Entry point
+* @c: the value passed to the function for checking
 * Description: function that checks for alphabetic character
 * Return: 1 if true, 0 if false
 */
