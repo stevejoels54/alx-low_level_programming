0ba60a4de1acdeb0be15120719d31c65cc75e09c
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-21 18:38:54 +0300

    Fixed 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -25,6 +25,11 @@ _putchar(':');
 _putchar(k + '0');
 _putchar(l + '0');
 _putchar('\n');
+/**Stop the loop when 23 : 59 is printed*/
+if (i == 2 && j == 3 && k == 5 && l == 9)
+{
+exit(0);
+}
 }
 }
 }
