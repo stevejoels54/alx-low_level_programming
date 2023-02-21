21ef87feb44451b2092aa11f54eb6b6c1237c1e3
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-21 14:36:42 +0300

    Added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+
+/**
+* main - main function of program
+* Description: prints "0-putchar.c"
+* Return: 0
+*/
+
+int main(void)
+{
+char string = "_putchar\n";
+while (string[i] != "\0")
+{
+_putchar(string[i]);
+i++;
+}
+return (0);
+}
