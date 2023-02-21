26ec8ef3db879be4edff0c841b39f7d50d10f50a
Joel Steven Ssekyewa <63671647+stevejoels54@users.noreply.github.com>
2023-02-21 16:59:41 +0300

    Fixed betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - prototype
+* _islower - Entry point
 * @c: value passed to function for checking
 * Description:  function that checks for lowercase character
 * Return: 1 if true, 0 if false
