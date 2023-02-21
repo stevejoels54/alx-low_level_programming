98b2b5dba82e639525ed0793b7ff42bf7a0a3725
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-21 16:19:22 +0300

    Added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - prototype
+* Description:  function that checks for lowercase character
+* Return: 1 if true, 0 if false
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
