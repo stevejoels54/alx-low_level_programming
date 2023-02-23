0072b1480f5cb0bbcea3fa4a3e74d08c76d57957
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-23 13:13:09 +0300

    Fixed 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,20 +9,19 @@
 
 void more_numbers(void)
 {
-int i, j;
+int i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
+k = j;
 if (j > 9)
 {
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
+putchar(j / 10 + '0');
+k = j % 10;
 }
-else
-{
-_putchar(j + '0');
-}
+
+putchar(k + '0');
 }
 _putchar('\n');
 }
