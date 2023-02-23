1547a9b46cb92f6e7c1e83189aa7e3ca99f34479
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-23 12:16:00 +0300

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - Entry point
+* @a: first number
+* @b: second number
+* Description: function that multiplies two integers a,b
+* Return: product (a*b)
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
