7c5b29625154f46e4eeebc8c6720ea878ce0ce0d
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-23 18:24:50 +0300

    Fixed 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,20 +11,19 @@ int main(void)
 {
 long int n = 612852475143;
 long int factor = 2;
-    
+long int largest_factor = 1;
 while (n > 1)
 {
 if (n % factor == 0)
 {
 n = n / factor;
+largest_factor = factor;
 }
 else
 {
 factor++;
 }
 }
-    
-printf("%ld\n", factor);
-    
+printf("%ld\n", largest_factor);
 return (0);
 }
