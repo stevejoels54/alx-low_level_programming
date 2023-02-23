9255a396b52044bfd3d0934f55ff2bebd9a65726
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-23 15:50:17 +0300

    Added 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,7 @@ void print_square(int size)
 int i, j;
 if (size <= 0)
 {
-putchar('\n');
+_putchar('\n');
 }
 else
 {
@@ -20,9 +20,9 @@ for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
-putchar('#');
+_putchar('#');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
 }
