b9cc549c3068d10ccbc31e340c63ddf3be5ed74b
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-28 00:59:21 +0300

    Added 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,12 @@
 
 void puts_half(char *str)
 {
-int i, len = 0;
+int i, len, start = 0;
 while (str[len] != '\0')
 {
 len++;
 }
-int start = len % 2 == 0 ? len / 2 : (len - 1) / 2 + 1;
+start = len % 2 == 0 ? len / 2 : (len - 1) / 2 + 1;
 for (i = start; i < len; i++)
 {
 _putchar(str[i]);
