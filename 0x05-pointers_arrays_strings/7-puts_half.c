2a27e8e9a36f2c2e6737259ccaedc4b78b7b4979
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-28 01:25:45 +0300

    Fixed 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,14 @@ while (str[len] != '\0')
 {
 len++;
 }
-start = len % 2 == 0 ? len / 2 : (len - 1) / 2 + 1;
+if (len % 2 == 0)
+{
+start = len / 2;
+}
+else
+{
+start = (len - 1) / 2 + 1;
+}
 for (i = start; i < len; i++)
 {
 _putchar(str[i]);
