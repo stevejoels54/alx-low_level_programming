d80cbc99b68afb9440b81744455e7ada71eb309c
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-02-28 03:09:19 +0300

    Added 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ * Description:  program that generates random valid
+ * passwords for the program 101-crackme.
+ * Return: Always zero
+ */
+
+#define PASSWORD_LENGTH 6
+#define CHARACTERS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+int main(void)
+{
+  char password[PASSWORD_LENGTH + 1];
+  int i, index;
+
+  // Seed the random number generator with the current time
+  srand(time(NULL));
+
+  // Generate a random password
+  for (i = 0; i < PASSWORD_LENGTH; i++) {
+    index = rand() % (sizeof(CHARACTERS) - 1);
+    password[i] = CHARACTERS[index];
+  }
+  password[PASSWORD_LENGTH] = '\0';
+
+  printf("%s\n", password);
+
+  return (0);
+}
