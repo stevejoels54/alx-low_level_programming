5f43c73408e0a7e048d9479f53144e4bcee9f613
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-03-03 00:19:27 +0300

    Fixed 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,40 +14,41 @@ char *strings_add(char *n1, char *n2, char *r, int r_index);
 
 char *strings_add(char *n1, char *n2, char *r, int r_index)
 {
-    int num, tens = 0;
+int num, tens = 0;
 
-    for (; *n1 && *n2; n1--, n2--, r_index--)
-    {
-        num = (*n1 - '0') + (*n2 - '0');
-        num += tens;
-        *(r + r_index) = (num % 10) + '0';
-        tens = num / 10;
-    }
+for (; *n1 && *n2; n1--, n2--, r_index--)
+{
+num = (*n1 - '0') + (*n2 - '0');
+num += tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
 
-    for (; *n1; n1--, r_index--)
-    {
-        num = (*n1 - '0') + tens;
-        *(r + r_index) = (num % 10) + '0';
-        tens = num / 10;
-    }
+for (; *n1; n1--, r_index--)
+{
+num = (*n1 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
 
-    for (; *n2; n2--, r_index--)
-    {
-        num = (*n2 - '0') + tens;
-        *(r + r_index) = (num % 10) + '0';
-        tens = num / 10;
-    }
+for (; *n2; n2--, r_index--)
+{
+num = (*n2 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
 
-    if (tens && r_index >= 0)
-    {
-        *(r + r_index) = (tens % 10) + '0';
-        return (r + r_index);
-    }
+if (tens && r_index >= 0)
+{
+*(r + r_index) = (tens % 10) + '0';
+return (r + r_index);
+}
 
-    else if (tens && r_index < 0)
-        return (0);
-
-    return (r + r_index + 1);
+else if (tens && r_index < 0)
+{
+return (0);
+}
+return (r + r_index + 1);
 }
 /**
  * infinite_add - Adds two numbers.
@@ -60,20 +61,25 @@ char *strings_add(char *n1, char *n2, char *r, int r_index)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int index, n1_len = 0, n2_len = 0;
+int index, n1_len = 0, n2_len = 0;
 
-    for (index = 0; *(n1 + index); index++)
-        n1_len++;
+for (index = 0; *(n1 + index); index++)
+{
+n1_len++;
+}
 
-    for (index = 0; *(n2 + index); index++)
-        n2_len++;
+for (index = 0; *(n2 + index); index++)
+{
+n2_len++;
+}
 
-    if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-        return (0);
+if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+{
+return (0);
+}
 
-    n1 += n1_len - 1;
-    n2 += n2_len - 1;
-    *(r + size_r) = '\0';
-
-    return (strings_add(n1, n2, r, --size_r));
+n1 += n1_len - 1;
+n2 += n2_len - 1;
+*(r + size_r) = '\0';
+return (strings_add(n1, n2, r, --size_r));
 }
