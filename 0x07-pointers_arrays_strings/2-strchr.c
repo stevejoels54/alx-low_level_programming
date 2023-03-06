6696be8e33477b72b8f318f0c28b510c364d6b40
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-03-06 21:22:01 +0300

    Fixed 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 /**
  * *_strchr - locates a character in a string
  * @s: string
- * @c: character to find 
+ * @c: character to find
  * Return: pointer to the first occurrence of the character c in the string s
  * or NULL if the character is not found
  */
