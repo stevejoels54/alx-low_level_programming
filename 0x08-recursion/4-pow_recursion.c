558b372a4e38d84388ecf4b9ff97ea1dca0fdd05
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-03-08 20:14:23 +0300

    Added 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number.
+ * @x: value whose power to raise
+ * @y: power to x to
+ * Return: If y is lower than 0, the function should return -1
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
