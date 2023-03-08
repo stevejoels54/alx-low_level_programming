aedd64173a2b9099afd9b6b32707814b4da5eb44
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-03-08 19:31:23 +0300

    Fixed betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,6 +3,8 @@
 /**
  * factorial - function that returns the factorial of a given number.
  * @n: Number to find factorial for.
+ * Return: n > 0 - the factorial of n
+ * n < 0 - 1 to indicate an error
  */
 
 int factorial(int n)
