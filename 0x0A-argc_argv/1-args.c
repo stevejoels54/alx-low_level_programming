7119844541d4742c285a3b0dd1f13bca5e4767d4
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-03-11 05:51:48 +0300

    Added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: Zero always
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; argv[0][i] != '\0'; i++)
+{
+putchar(argv[0][i]);
+}
+putchar('\n');
+return 0;
+}
