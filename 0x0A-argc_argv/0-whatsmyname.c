ec191fd3e7a45dc2b4a9db705a4b462d436e515b
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-03-11 05:33:04 +0300

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into main
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: Zero always
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
