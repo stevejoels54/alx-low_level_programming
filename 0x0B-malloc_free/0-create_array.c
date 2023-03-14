84f45f402b3f79c2f917f989a5bca0f45e46434f
Steve Joels <63671647+stevejoels54@users.noreply.github.com>
2023-03-14 13:36:27 +0300

    Added 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,22 +12,25 @@
 
 char *create_array(unsigned int size, char c)
 {
-unsigned int i;
-if (size == 0)
-{
-return (NULL);
-}
+	unsigned int i;
+	char *arr;
 
-char *arr = malloc(size * sizeof(char));
-if (arr == NULL)
-{
-return (NULL);
-}
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-for (i = 0; i < size; i++)
-{
-arr[i] = c;
-}
+	arr = malloc(size * sizeof(char));
 
-return (arr);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
 }
