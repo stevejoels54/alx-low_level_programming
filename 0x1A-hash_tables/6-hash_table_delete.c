1ef77adb85144d8df960c0eabbc9d641f090fd81
joelsteven <joelofelectronics@gmail.com>
2023-07-08 02:43:44 +0300

    Added 6 delete task

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *current = ht->array[i];
+
+		while (current != NULL)
+		{
+			hash_node_t *temp = current;
+
+			current = current->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
