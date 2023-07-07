fabf52b3d9d828cf9c544335ebd7c2cb839a31ff
joelsteven <joelofelectronics@gmail.com>
2023-07-08 02:34:26 +0300

    Fixed 5 print

--- 0x1A-hash_tables/5-test.c
+++ 0x1A-hash_tables/5-test.c
@@ -4,14 +4,14 @@
  * hash_table_print - Prints a hash table.
  * @ht: A pointer to the hash table to print.
  *
- * Description: Key: value pairs printed in order
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
-	unsigned char print_comma = 0;
 	unsigned long int i;
+	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
 		return;
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[i] != NULL)
 		{
-			if (print_comma == 1)
+			if (comma_flag == 1)
 				printf(", ");
 
 			node = ht->array[i];
@@ -29,10 +29,11 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				printf("'%s': '%s'", node->key, node->value);
 				node = node->next;
+				if (node != NULL)
+					printf(", ");
 			}
-			print_comma = 1;
+			comma_flag = 1;
 		}
 	}
 	printf("}\n");
 }
-
